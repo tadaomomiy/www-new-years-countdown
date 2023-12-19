5cd77592e40302efafd90a0d0ebf409c42953a00
Matheus Momiy <matheus.tadao@gmail.com>
2023-12-19 14:43:34 +0900

    Added a new function on Counter to get the timezone, added 2 propertys in Counter component to ajust the timezone and a label

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,19 @@ import { Header } from './components/Header';
 import './global.css';
 
 export function App() {
+  function getLocalTimezone() {
+    const date = new Date();
+    const localTimezone = date.getTimezoneOffset() / 60 * (-1);
+    return localTimezone
+  }
 
   return (
     <div className={ styles.card }>
       <Header />
-      <Counter />
+      <Counter placeName={"Local Timezone"} timezone={getLocalTimezone()}/>
+      <Counter placeName={"Brasil"} timezone={-3}/>
+      <Counter placeName={"Japan"} timezone={+9}/>
+      <Counter placeName={"London"} timezone={0}/>
     </div>
   )
 };
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,13 +2,25 @@ import { useState, useEffect } from 'react';
 
 import styles from './Counter.module.css'
 
-export function Counter() {
+export function Counter( { timezone, placeName }) {
     const [days, setDays] = useState(0);
     const [hours, setHours] = useState(0);
     const [minutes, setMinutes] = useState(0);
     const [seconds, setSeconds] = useState(0);
 
     const newYear = new Date("Januay, 01, 2024");
+
+    function getDateWithTimezone(timezone) {
+      const date = new Date();
+      const localTime = date.getTime();
+      const localOffset = date.getTimezoneOffset() * 60000;
+      const utc = localTime + localOffset;
+      const offset = timezone
+      const utcWithTimezone = utc + (3600000 * offset);
+      const dateNow = new Date(utcWithTimezone);
+
+      return dateNow
+    }
 
     function getNewYear(remainingDays) {
         if (remainingDays < 0){
@@ -22,7 +34,7 @@ export function Counter() {
     }
 
     const getTime = (newYear) => {
-        const newYearFromNowInSeconds = Date.parse(newYear) - Date.now();
+        const newYearFromNowInSeconds = Date.parse(newYear) - getDateWithTimezone(timezone);
         const remainingDays = newYearFromNowInSeconds / (1000 * 60 * 60 * 24);
         const newYearFromNowInDays = getNewYear(remainingDays);
 
@@ -51,28 +63,28 @@ export function Counter() {
       {
         id: 1,
         date: {
-          type: 'Day',
+          type: 'Days',
           value: days,
         }
       },
       {
         id: 2,
         date: {
-          type: 'Hour',
+          type: 'Hours',
           value: hours,
         }
       },
       {
         id: 3,
         date: {
-          type: 'Min',
+          type: 'Mins',
           value: minutes
         }
       },
       {
         id: 4,
         date: {
-          type: 'Sec',
+          type: 'Secs',
           value: seconds
         }
       }
@@ -80,6 +92,7 @@ export function Counter() {
 
     return (
         <div>
+          <header className={ styles.tittle }>{placeName}</header>
             <div className={ styles.wrapper }>
                 {counters.map(counter => {
                     return (
